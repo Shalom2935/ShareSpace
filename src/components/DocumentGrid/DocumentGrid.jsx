3d20fbcd7a3d3c8e5d86a1f05dc60b18822953b8
Shalom <kamokoueshalom@gmail.com>
2024-08-12 13:53:54 +0200

    Home page layout

--- src/components/DocumentGrid/DocumentGrid.jsx
+++ src/components/DocumentGrid/DocumentGrid.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import axios from 'axios'
 import { useEffect, useState } from 'react'
 import DocumentCard from '../DocumentCard/DocumentCard'
+import './DocumentGrid.scss'
 
 
 const DocumentGrid = () => {
