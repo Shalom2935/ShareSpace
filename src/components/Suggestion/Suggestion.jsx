c7aba7c333a843e4b90f4a6de9ee2f600f958a0a
Shalom <kamokoueshalom@gmail.com>
2024-08-13 18:04:43 +0200

    add auto-suggestion

---
+++ src/components/Suggestion/Suggestion.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react'
+import { fetchDocuments } from '../../utils/fetchDocuments'
+import { filterDocuments } from '../../utils/filterDocuments'
+import './Suggestion.scss'
+
+function Suggestion({ searchTerm, isFocus }) {
+    const [list, setList] = useState([])
+    const [filteredList, setFilteredList] = useState([])
+
+    useEffect(() => {
+        const loadList = async () => {
+        setList(await fetchDocuments())    
+        }
+        loadList()
+    },[])
+
+    useEffect(() => {
+      const filterSearch = () => {
+      setFilteredList(searchTerm && isFocus ? filterDocuments(list,searchTerm) : [])
+      }
+      filterSearch()
+    
+    },[searchTerm, list, isFocus])
+
+
+  return (
+    <div className='suggestion'>
+        {filteredList.map(e => (
+            <div className="suggestion_item" key={e.id}>{e.title}</div>
+        ))}
+    </div>
+  )
+}
+
+export default Suggestion
--- src/utils/fetchDocuments.js
+++ src/utils/fetchDocuments.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const fetchDocuments = async () => {
   try {
-    const response = await axios.get('https://66d8dd5c-b863-46a9-a7f4-25163daa6c2b.mock.pstmn.io/Test');
+    const response = await axios.get('http://localhost:3001/Test/files');
     return response.data;
   } catch (error) {
     console.error('There was an error fetching the documents!', error);
