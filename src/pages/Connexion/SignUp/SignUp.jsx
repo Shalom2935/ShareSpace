46ca3369730653192a64d4a8781fe78c4b52292a
Shalom <kamokoueshalom@gmail.com>
2024-09-05 15:43:46 +0200

    password handling

--- src/pages/Connexion/SignUp/SignUp.jsx
+++ src/pages/Connexion/SignUp/SignUp.jsx
@@ -1,15 +1,31 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './SignUp.scss'
 import { handleSignUp } from './handleSignUp'
 import OnLoad from '../onLoad/onLoad'
 
 function SignUp({ showLogin }) {
+ const [password, setPassword] = useState('');
+ const [confirmPassword, setConfirmPassword] = useState('');
   const [onLoad, setOnLoad] = useState(false);
   const [errors, setErrors] = useState({
     matriculeError: '',
     emailError: '',
-    passwordError: ''
+    passwordError: '',
+    confirmPasswordError: ''
   });
+
+  useEffect(() => {
+    
+    if (password.length > 0 && password.length < 8 ) {
+      setErrors({passwordError : 'Password must be at least 8 characters'});
+    } else {
+      setErrors({passwordError : ''});
+    }
+
+    if (confirmPassword.length && (password !== confirmPassword)) {
+      setErrors({ confirmPasswordError : 'Passwors do not match' });
+    }
+  }, [password, confirmPassword]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -18,19 +34,23 @@ function SignUp({ showLogin }) {
     const formData = new FormData(e.target);
     const data = Object.fromEntries(formData);
 
+    delete data.confirmPassword;
+
     try {
       await handleSignUp(data);
 
       setErrors({
         matriculeError: '',
         emailError: '',
-        passwordError: ''
+        passwordError: '',
+        confirmPasswordError: ''
       });
     } catch (error) {
       setErrors({
         matriculeError: error.matricule || '',
         emailError: error.email || '',
-        passwordError: error.password || ''
+        passwordError: error.password || '',
+        confirmPasswordError: ''
       });
     } finally {
       setOnLoad(false);
@@ -50,19 +70,38 @@ function SignUp({ showLogin }) {
           <div className="signup_form__matricule">
             <label htmlFor="matricule">Matricule</label>
             <div className='wrapper'>
-            <input type="text" name='matricule' style={ errors.matriculeError ? {border: '2px solid red'} : {}} />
-            {errors.matriculeError && <p className="signuperror">{errors.matriculeError}</p>}                     
+              <input type="text" name='matricule' style={ errors.matriculeError ? {border: '2px solid red'} : {}} />
+              {errors.matriculeError && <p className="signuperror">{errors.matriculeError}</p>}                     
             </div>
          </div>
           <div className="signup_form__mail">
             <label htmlFor="email">Email</label>
-            <input type="email" name='email' style={ errors.emailError ? {border: '1px solid red'} : {}} />
-            {errors.emailError && <p className="emailerror">{errors.emailError}</p>}            
-          </div>
+            <div className="wrapper">
+              <input type="email" name='email' style={ errors.emailError ? {border: '2px solid red'} : {}} />
+              {errors.emailError && <p className="signuperror">{errors.emailError}</p>}                
+            </div>
+         </div>
           <div className="signup_form__password">
             <label htmlFor="password">Password</label>
-            <input type="password" name='password' style={ errors.passwordError ? {border: '1px solid red'} : {}} />
-            {errors.passwordError && <p className="passworderror">{errors.passwordError}</p>}            
+            <div className="wrapper">
+              <input 
+                type="password" 
+                name='password'
+                onChange={(e) => {setPassword(e.target.value)}} 
+                style={ errors.passwordError || errors.confirmPasswordError ? {border: '2px solid red'} : {}} />
+              {errors.passwordError && <p className="signuperror">{errors.passwordError}</p>}            
+            </div>
+          </div>
+          <div className="signup_form__confirmPassword">
+            <label htmlFor="confirmPassword">Confirm Password</label>
+            <div className="wrapper">
+              <input 
+                type="password" 
+                name='confirmPassword'
+                onChange={(e) => {setConfirmPassword(e.target.value)}}  
+                style={ errors.confirmPasswordError ? {border: '2px solid red'} : {}} />
+              {errors.confirmPasswordError && <p className="signuperror">{errors.confirmPasswordError}</p>}            
+            </div>
           </div>
           <p>Already have an account? <span onClick={showLogin}>Login</span></p>
           <button 
